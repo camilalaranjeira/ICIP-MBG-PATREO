360150b3b3dea02662d02b7e716b9cde8c89cc18
camilalaranjeira <mila.laranjeira@gmail.com>
2023-04-05 16:50:43 +0000

    updating setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 apt-get update
-apt-get install -y libsm6 libxrender-dev
 pip install pandas lxml seaborn 
 rm -r /usr/include/opencv*
 rm -r /usr/include/boost/compute/interop/opencv*
@@ -8,3 +7,4 @@ rm -r /usr/share/licenses/opencv3
 rm /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
 rm /usr/lib/x86_64-linux-gnu/libopencv*  
 pip install opencv-python==4.2.0.34
+apt-get install -y libsm6 libxext6 libxrender-dev
